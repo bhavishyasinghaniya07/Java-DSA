85694750b19b248ddb3538054cd01826b99f23ad
Bhavishya Parmar <parmarbhavishya@gmail.com>
2025-06-14 08:45:38 +0530

    Updated Prime

--- Z_Practice/Prime.java
+++ Z_Practice/Prime.java
@@ -1,19 +1,30 @@
 import java.util.*;
 
-class Prime {
+public class Prime {
   public static boolean isPrime(int n) {
-    if (n == 1 || n == 2)
-      return true;
+    if (n == 1)
+      return false;
+    if (n % 2 == 0)
+      return false;
 
-    for (int i = 2; i < Math.sqrt(n); i++) {
+    for (int i = 2; i <= Math.sqrt(n); i++) {
       if (n % i == 0)
-        return true;
+        return false;
     }
-    return false;
+    return true;
+  }
+
+  public static void printPrime(int n) {
+
+    for (int i = 2; i <= n; i++) {
+      if (isPrime(i))
+        System.out.print(i + " ");
+    }
   }
 
   public static void main(String[] args) {
     int n = 73;
     System.out.println(isPrime(n));
+    printPrime(n);
   }
 }
